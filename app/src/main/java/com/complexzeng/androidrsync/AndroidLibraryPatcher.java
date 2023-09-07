1dd865923991f094abbacd95adaa9d1f64c32a3d
complexzeng <complexzeng@tencent.com>
2023-09-07 14:52:47 +0800

    init

---
+++ app/src/main/java/com/complexzeng/androidrsync/AndroidLibraryPatcher.java
@@ -0,0 +1,74 @@
+package com.complexzeng.androidrsync;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AndroidLibraryPatcher {
+    private static final String TAG = "AndroidLibraryPatcher";
+
+    static void start(Context ctx) {
+        Thread thread = new Thread(() -> startInternal(ctx));
+        thread.start();
+    }
+
+    private static void startInternal(Context ctx) {
+        String libraryPath = ctx.getApplicationInfo().nativeLibraryDir;
+
+        File libraryDir = new File(libraryPath);
+        boolean canWrite = libraryDir.canWrite();
+        boolean canRead = libraryDir.canRead();
+        boolean canExecute = libraryDir.canExecute();
+        Log.d(TAG, "libraryDir: " + libraryDir.getAbsolutePath() + ", canWrite: " + canWrite + ", canRead: " + canRead + ", canExecute: " + canExecute);
+
+        File rsyncOutputPath = ctx.getCacheDir();
+        boolean rsyncOutputPathCanWrite = rsyncOutputPath.canWrite();
+        boolean rsyncOutputPathCanRead = rsyncOutputPath.canRead();
+        boolean rsyncOutputPathCanExecute = rsyncOutputPath.canExecute();
+        Log.d(TAG, "rsyncOutputPath: " + rsyncOutputPath.getAbsolutePath() + ", canWrite: " + rsyncOutputPathCanWrite + ", canRead: " + rsyncOutputPathCanRead + ", canExecute: " + rsyncOutputPathCanExecute);
+
+        File configFile = new File(rsyncOutputPath, "rsyncd.conf");
+
+        try {
+            boolean success = configFile.createNewFile();
+            Log.d(TAG, "create rsyncd.conf res= " + success);
+            String config = "address = 127.0.0.1\n" +
+                    "port = 1873\n" +
+                    "[root]\n" +
+                    "path = /\n" +
+                    "use chroot = false\n" +
+                    "read only = false\n";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(configFile));
+            writer.write(config);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        File rsyncExec = new File(libraryPath, "librsync.so");
+        Runtime rt = Runtime.getRuntime();
+        try {
+            Process process = rt.exec(rsyncExec.getAbsolutePath() + " --daemon --no-detach --config=" + configFile.getAbsolutePath() + " --log-file=/proc/self/fd/2");
+            BufferedReader outputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = outputReader.readLine()) != null) {
+                Log.d(TAG, line);
+            }
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            while ((line = errorReader.readLine()) != null) {
+                Log.e(TAG, line);
+            }
+            int exitCode = process.waitFor();
+            Log.d(TAG, "exitCode: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ app/src/main/java/com/complexzeng/androidrsync/MainActivity.java
@@ -0,0 +1,21 @@
+package com.complexzeng.androidrsync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AndroidLibraryPatcher.start(getApplicationContext());
+    }
+}
