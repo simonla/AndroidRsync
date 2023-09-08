6dafbadd4091c05d044a7bc0491fee5ffc2653dd
complexzeng <complexzeng@tencent.com>
2023-09-08 19:00:58 +0800

    fix

--- app/src/main/java/com/complexzeng/androidrsync/MainActivity.java
+++ app/src/main/java/com/complexzeng/androidrsync/MainActivity.java
@@ -85,6 +85,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        observer.stopWatching();
+//        observer.stopWatching();
     }
 }
